62f75070bf78a449b9665c29d9e16326074c444e
Terry Moreland <tsmoreland@live.com>
2020-02-20 20:06:59 +0000

    correction 1 of module 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,13 +5,13 @@ let quizData = require('./quiz_data.json')
 class Quiz extends Component {
     constructor(props) {
         super(props)
+        this.state = { quiz_position: 1 }
     }
     render() {
         return (
-            <div><div class="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
+            <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
         )
     }
-
 }
 
 export default Quiz
