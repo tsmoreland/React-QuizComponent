3c6239d6cff6252b2313804529a1fafedfd1857e
Terry Moreland <tsmoreland@live.com>
2020-02-20 20:39:10 +0000

    removed useless constructor

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 
 class QuizQuestion extends Component {
-    constructor(props) {
-        super(props)
-    }
     render() {
         return (
             <main>
